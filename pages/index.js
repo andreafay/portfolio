ca4181462b45f11f3154cfc35fba107c3599adcb
andrea.fay <andrea.fay@esternibisp.com>
2023-02-27 11:23:03 +0100

    added profile pic

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
-        Hello, I&apos;m a full-stack developer based in Italy!
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} mb={6} p={3} align="center">
+        Hello, I&apos;m a fullstack developer based in Italy!
       </Box>
 
       <Box display={{md:'flex'}}>
@@ -13,6 +13,10 @@ const Page = () => {
             Andrea Fay
           </Heading>
           <p>WebDev, Cryptos, Sports & Music</p>
+        </Box>
+        <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
+          <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid"
+                 maxW="100px" maxH="100px" borderRadius="full" display="inline-block" src="/images/propic.png" alt="Profile Image" />
         </Box>
       </Box>
     </Container>
