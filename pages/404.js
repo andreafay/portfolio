6c6e7890098c0a07e0552da1de093d19a54a06ad
andrea.fay <andrea.fay@esternibisp.com>
2023-03-06 20:02:19 +0100

    added transition on 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,29 @@
 import Link from 'next/link'
 import {
-  Box,
-  Heading,
-  Text,
-  Container,
-  Divider,
-  Button
+    Box,
+    Heading,
+    Text,
+    Container,
+    Divider,
+    Button
 } from '@chakra-ui/react'
+import Layout from '@/components/layouts/article'
 
 const NotFound = () => {
-  return (
-    <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Divider my={6} />
-      <Box my={6} align="center">
-        <Button as={Link} href="/" colorScheme="teal">
-          Return to home
-        </Button>
-      </Box>
-    </Container>
-  )
+    return (
+        <Layout>
+            <Container>
+                <Heading as="h1">Not found</Heading>
+                <Text>The page you&apos;re looking for was not found.</Text>
+                <Divider my={6} />
+                <Box my={6} align="center">
+                    <Button as={Link} href="/" colorScheme="teal">
+                        Return to home
+                    </Button>
+                </Box>
+            </Container>
+        </Layout>
+    )
 }
 
 export default NotFound
