769a9aa657106df827b5f310ae224fff7a6c8150
Andrea Fay <andrea.fay@esternibisp.com>
2024-06-05 17:28:31 +0200

    center footer text

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import Paragraph from "./paragraph"
 
 const Footer = () => {
     return (
-        <p className="opacity-40 text-xs mt-10">
+        <p className="opacity-40 text-xs text-center mt-10">
           This website is a reproduction of craftz.dog by Takuya Matsuyama.
         </p>
     )
