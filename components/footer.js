60377ced9a9886ea9f5774a3618d0b7fdabe6577
Andrea Fay <andrea.fay@esternibisp.com>
2024-06-05 17:01:24 +0200

    small text css update

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,9 @@ import Paragraph from "./paragraph"
 
 const Footer = () => {
     return (
-        <Paragraph className="opacity-40 text-xs text-center font-semibold mt-10">
+        <p className="opacity-40 text-xs mt-10">
           This website is a reproduction of craftz.dog by Takuya Matsuyama.
-        </Paragraph>
+        </p>
     )
 }
 
